b22441c4b26e6ef8bd0487048ab9c407f2bbda95
hahey <heynaheyna9@gmail.com>
2020-10-25 22:51:55 +0100

    clean echo messages

--- ssidconnect.sh
+++ ssidconnect.sh
@@ -13,18 +13,16 @@ fi
 
 ssidlist=( $(sudo iwlist wlp1s0 scan| fgrep 'ESSID:"MikroTik' | grep -o '".*"'|uniq| tr "\n" " ") )
 echo "${#ssidlist[@]}"
+echo "going through the following ssids to connect:"
 echo "${ssidlist[@]}"
 i=0
 for ssidq in "${ssidlist[@]}"; do
     [ -f $HOME/.ssh/known_hosts ] && rm $HOME/.ssh/known_hosts
     i=$((i+1))
-    echo "${ssidq}"
     ssid=$(echo $ssidq | tr -d '"')
-    echo "${ssid}"
     nmcli device wifi connect "${ssid}"
     ssh_target=`ip r|grep -oP "via .* dev"|grep -oe '\([0-9.]*\)'`
-    echo "${ssh_target}"
-    echo "idnum ${i}"
+    echo "floor-idnum $2-${i} : ${ssid}"
     { echo; cat ./scripts/$3 | tr "\n" " "; echo; } | street=$1 floor=$2 idnum=$i envsubst '$street,$floor,$idnum' | ssh -oStrictHostKeyChecking=no -l admin "${ssh_target}"
     ssh -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -oServerAliveInterval=10 -l admin "${ssh_target}" '/system script run ffconfig'
 done
