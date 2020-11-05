238a5ba2ced1583beae989eda6fbb6a1167c7440
hahey <heynaheyna9@gmail.com>
2020-11-05 11:55:52 +0100

    remove only a specific keyfrom the known host key

--- ssidconnect.sh
+++ ssidconnect.sh
@@ -17,11 +17,11 @@ echo "going through the following ssids to connect:"
 echo "${ssidlist[@]}"
 i=0
 for ssidq in "${ssidlist[@]}"; do
-    [ -f $HOME/.ssh/known_hosts ] && rm $HOME/.ssh/known_hosts
     i=$((i+1))
     ssid=$(echo $ssidq | tr -d '"')
     nmcli device wifi connect "${ssid}"
     ssh_target=`ip r|grep -oP "via .* dev"|grep -oe '\([0-9.]*\)'`
+    ssh-keygen -R "$ssh_target"
     echo "floor-idnum $2-${i} : ${ssid}"
     { echo; cat ./scripts/$3 | tr "\n" " "; echo; } | street=$1 floor=$2 passwd=$4 idnum=$i envsubst '$street,$floor,$passwd,$idnum' | ssh -oStrictHostKeyChecking=no -l admin "${ssh_target}"
     ssh -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -oServerAliveInterval=10 -l admin "${ssh_target}" '/system script run ffconfig'
